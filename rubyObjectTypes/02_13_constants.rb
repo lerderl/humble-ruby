014c298474742e035221bfbc1c6677a7b96e5e7b
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-04-27 08:42:00 +0000

    Understanding constants

---
+++ rubyObjectTypes/02_13_constants.rb
@@ -0,0 +1,14 @@
+MAX_SCORE = 100
+# => 100
+max_score = 100
+# => 100
+MAX_SCORE = 50
+# warning: already initialized constant
+# => 50
+MAX_SCORE
+# => 50
+Temp = 72
+# => 72
+Temp = 68
+# warning: already initialized constant
+# => 68
