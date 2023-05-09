dd1655a0511c1aee276cd67709c8eafbd4b6ce32
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-05-09 15:40:22 +0000

    iterator control structure

---
+++ controlStructures/iterators.rb
@@ -0,0 +1,13 @@
+5.times do |i|
+  puts "Countdown #{5-i}"
+end
+puts "Blast off!"
+
+5.downto(1) {|i| puts "Countdown #{i}" }
+puts "Blast off!"
+
+fruits = ['banana', 'apple', 'pear', 'Water melon']
+
+fruits.each do |fruit|
+  puts fruit.capitalize
+end
