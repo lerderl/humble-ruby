d446b102c822c6a7e7e7a62cd47a55390fd8e6ed
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-05-10 09:10:24 +0000

    update game

--- scripting/guessing_game.rb
+++ scripting/guessing_game.rb
@@ -1,31 +1,39 @@
 #!/usr/bin/env ruby
 
-# Ask user for their name
-# Say hello and explain rules
-# Pick a random number (rand(100))
-# Get the user's guess
-# Tell player if guess is correct
-# Loop to allow three guesses
+puts "----------------------"
+puts "| Ruby Guessing Game |"
+puts "----------------------"
 
-puts "------------------------"
-puts "|  RUBY GUESSING GAME  |"
-puts "------------------------"
-
-print "Enter your name: "
+puts "What is your name?"
+print "> "
 name = gets.chomp
 
-puts "Hello, #{name}!\nWELCOME TO THE RUBY GUESSING GAME"
-print "I will pick a random number from 1 to 100\nand you will have three chances to guess it\n"
+puts "Hello, #{name}."
+puts "We are going to play a guessing game."
+puts "I will choose a random number between 1 and 10"
+puts "and you will have three chances to guess it."
 
-number = rand(100)
-game_number = number + 1
-puts "Okay, I have chosen my number.\nIt's your turn!"
+number = rand(10) + 1
+puts "Okay, I have my number."
 
-3.times do |i|
-    print "Guess #{i + 1}: "
-    player_number = gets.chomp
+MAX_GUESSES = 3
 
-    puts player_number == game_number ? "Congratulations! You guessed right" : "Try again"
+1.upto(MAX_GUESSES) do |guess_num|
+  print "Guess #{guess_num}: "
+  guess = gets.chomp
+  if guess.to_i == number
+    puts "Great guessing, #{name}!"
+    puts "My number was #{number}."
+    break
+  else
+    puts "Sorry, that wasn't it."
+    if guess_num == MAX_GUESSES
+      puts
+      puts "That was your last guess."
+      puts "My number was #{number}."
+    end
+  end
+  
 end
 
-puts "Game Over! The correct number is #{game_number}"
+puts "\n\nGoodbye!"
---
+++ scripting/my_guessing_game.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+# Ask user for their name
+# Say hello and explain rules
+# Pick a random number (rand(100))
+# Get the user's guess
+# Tell player if guess is correct
+# Loop to allow three guesses
+
+puts "------------------------"
+puts "|  RUBY GUESSING GAME  |"
+puts "------------------------"
+
+print "Enter your name: "
+name = gets.chomp
+
+puts "Hello, #{name}!\nWELCOME TO THE RUBY GUESSING GAME"
+print "I will pick a random number from 1 to 10\nand you will have three chances to guess it\n"
+
+number = rand(10)
+game_number = number + 1
+puts "Okay, I have chosen my number.\nIt's your turn!"
+
+3.times do |i|
+  print "Guess #{i + 1}: "
+  player_number = gets.chomp
+
+  if player_number.to_i == game_number
+    puts "Congratulations! You guessed right"
+    break
+  else
+    puts i < 2 ? "Try again" : "Game Over! The correct number is #{game_number}"
+  end
+end
